3bf2ca332b02280d6873f89d8324457e43a93791
Karol Buchajczuk <omniquisi@gmail.com>
2019-05-02 15:52:03 +0200

    Something #1

--- uselesslib.js
+++ uselesslib.js
@@ -1,4 +1,12 @@
+// Rounding input to precision decimal places
 function roundUp(input, precision){
     var p = Math.pow(10, precision);
     return Math.round(input*p) / p;
+}
+
+// Clock with refreshfreq refresh frequency and output text
+function clock(refreshfreq){
+    var dt = new Date();
+    var output = dt.toLocaleTimeString();
+    setTimeout(clock, refreshfreq)
 }
