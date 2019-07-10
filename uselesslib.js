a82e1746ff3792305df73556dfb36d876b3a7c98
Karol Buchajczuk <omniquisi@gmail.com>
2019-07-10 17:46:12 +0200

    New functions added, rest updated

--- uselesslib.js
+++ uselesslib.js
@@ -1,12 +1,22 @@
 // Rounding input to precision decimal places
 function roundUp(input, precision){
-    var p = Math.pow(10, precision);
-    return Math.round(input*p) / p;
+    return Math.round(input*Math.pow(10, precision)) / p;
 }
 
 // Clock with refreshfreq refresh frequency and output text
 function clock(refreshfreq){
-    var dt = new Date();
-    var output = dt.toLocaleTimeString();
+    var output = new Date().toLocaleTimeString();
     setTimeout(clock, refreshfreq);
+}
+
+//Conversion Number->Array of 0-9 and Array of 0-9->Number
+function arrayToNumber(inarray){
+    var output = 0;
+    for(i=0; i<inarray.length; i++) output+=[inarray.length-1-i] * Math.pow(10,i);
+    return output;
+}
+
+function numberToArray(inNumber){
+    var output = Array.from(String(inNumber), Number);
+    return output;
 }
